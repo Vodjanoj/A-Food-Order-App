0cfbd1e71948966918f3374a88bc8fbf4c6723a9
Vodjanoj <vladimirs.vodjanojs@gmail.com>
2022-03-07 13:46:11 +0200

    Working with Refs & Forward Refs

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,33 @@
 import Input from "../../UI/Input";
+import { useRef, useState } from "react";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value; // currently entered value(always a string)
+    const enteredAmountNumber = +enteredAmount; // converting string to a number
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -12,10 +35,11 @@ const MealItemForm = (props) => {
           min: "1",
           max: "5",
           step: "1",
-          defaultValue: "1"
+          defaultValue: "1",
         }}
       />
       <button>+Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
   );
 };
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,14 +1,15 @@
+import React from 'react';
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => { // React.forwardRef is being used here becase we can't use ref in MealItemForm.js in Input custom component
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
       {/* {...props.input} This ensures that all the key value pairs, 
       which we receive on props.input are added as props to input. */}
     </div>
   );
-};
+});
 
 export default Input;
