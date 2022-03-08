d05e4416aa3cb212fbbc1e4fa0c373b07a4c0b02
Vodjanoj <vladimirs.vodjanojs@gmail.com>
2022-03-08 17:28:28 +0200

    Working on a More Complex Reducer Logic

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,32 +9,49 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
-
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item); // we don't want to edit our old state snapshot, instead we generate new brand state object
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
-      
+
+    // itemsfindIndex finds the index of an item in an array, it takes a function which should return true if that's the item we're looking for, and false otherwise
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    ); // if the item we're currently looking at in that array has the same id as the item we're adding
+    // with this action which was dispatched, return actually the index of that item if it exists.
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem) {
+      // if item is already a part of array
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item); // we don't want to edit our old state snapshot, instead we generate new brand state object
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
-      
     };
   }
- 
+
   return defaultCartState; // returning actually new state
 };
-
 
 const CartProvider = (props) => {
   const [cartState, dispatchCartAction] = useReducer(
     cartReducer,
-    defaultCartState 
+    defaultCartState
   );
- 
+
   const addItemToCartHandler = (item) => {
     dispatchCartAction({ type: "ADD", item: item });
-    
   };
 
   const removeItemFromCartHandler = (id) => {
